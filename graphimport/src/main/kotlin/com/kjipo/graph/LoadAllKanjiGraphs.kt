190967d512ded0afa2d90ed255133e4530dc811c
Tore Brede <tore.brede@gmail.com>
2019-06-22 23:15:58 +0200

    Working on extracting paths

--- graphimport/src/main/kotlin/com/kjipo/graph/LoadAllKanjiGraphs.kt
+++ graphimport/src/main/kotlin/com/kjipo/graph/LoadAllKanjiGraphs.kt
@@ -1,10 +1,10 @@
 package com.kjipo.graph
 
-import org.apache.tinkerpop.gremlin.process.traversal.IO
-import org.apache.tinkerpop.gremlin.process.traversal.IO.graphml
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
-import org.apache.tinkerpop.gremlin.structure.T
-import org.apache.tinkerpop.gremlin.structure.io.IoCore
+
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal.Symbols.select
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.`__`.outE
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.`__`.select
+import org.apache.tinkerpop.gremlin.structure.Edge
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -16,7 +16,7 @@ object LoadAllKanjiGraphs {
     private fun loadAll() {
         val graphList = mutableListOf<TinkerGraph>()
 
-        Files.list(Paths.get("/home/student/workspace/kanjiR/kanji_graphs")).forEach { graphMlFile ->
+        Files.list(Paths.get("/home/student/workspace/kanjiR/kanji_graphs2")).forEach { graphMlFile ->
             val newGraph = TinkerGraph.open()
             newGraph.traversal().io<Any>(graphMlFile.toAbsolutePath().toString())
                     .read()
@@ -27,13 +27,41 @@ object LoadAllKanjiGraphs {
 
         val graph = graphList[0]
 
+
 //        println("Graph: ${graph.features()}")
 
-        val traversals = graph.traversal().withComputer().V().shortestPath().toList()
+        val traversals = graph.traversal()
+                .withComputer()
+//                .E()
+                .V()
+                .repeat(
+                        outE().`as`("e")
+                                .inV()).times(5)
+//                .shortestPath()
+                .path()
+//                .by("e")
+//                .select<Any>("e")
+//                .values<Any>("category")
+//                .outE("e_category")
+//                .outE()
+//                .path()
+//                .by("category")
+//                .group<Any, Any>()
+//                .select<Any>("e")
+//                .label()
+//                .by(select<Any, Any>("e"))
+//                        .values<Any>("category"))
+//                .valueMap<Any>()
+//                .toList()
 
+        for (traversal in traversals) {
+            val filteredPath = traversal.filter { it is Edge }
+                    .map { it as Edge }
+                    .map { graph.edges(it.id()).next().property<Any>("category").value() }
 
-        println(traversals)
-
+//            println("Traversal: $traversal")
+            println("Filtered path: $filteredPath")
+        }
 
     }
 
