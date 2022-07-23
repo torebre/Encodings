18e082a4f40fb4ebae65a99e092da59037a68242
Tore Brede <tore.brede@gmail.com>
2022-07-23 16:47:29 +0200

    Working on moving some methods over from Python project

---
+++ experiments/src/main/kotlin/com/kjipo/datageneration/CreateSamples.kt
@@ -0,0 +1,189 @@
+package com.kjipo.datageneration
+
+import com.kjipo.prototype.LinePrototype
+import java.lang.Math.pow
+import kotlin.math.*
+import kotlin.random.Random
+
+
+class LinePrototypeWithAngle(startPair: com.kjipo.raster.segment.Pair,
+stopPair: com.kjipo.raster.segment.Pair, val angle: Double, val length: Double): LinePrototype(startPair, stopPair) {
+
+}
+
+
+
+object CreateSamples {
+
+    private val random = Random.Default
+
+    fun generateSample(
+        numberOfSamplesToGenerate: Int, includeRectangle: Boolean,
+        numberOfRows: Int, numberOfColumns: Int, numberOfRandomLines: Int
+    ): List<LinePrototypeWithAngle> {
+        val sample = mutableListOf<LinePrototypeWithAngle>()
+
+        // Include four more lines if no rectangle should be drawn, so that the number of
+        // lines returned is the same
+        val numberOfLines = numberOfSamplesToGenerate + if(!includeRectangle) 4 else 0
+
+
+//        training_samples = np.zeros((number_of_random_lines + 4, 6))
+//
+//        if not include_rectangle:
+//        # Include four more random lines so that the returned number of lines is always the same
+//                number_of_random_lines += 4
+//
+//        for i in range(0, number_of_random_lines):
+//        # TODO Check if end is inclusive for range
+//        start_x = random.sample(range(0, number_of_rows), 1)[0]
+//        start_y = random.sample(range(0, number_of_columns), 1)[0]
+
+        val xStart = random.nextInt(0, numberOfRows)
+        val yStart = random.nextInt(0, numberOfColumns)
+
+        val xStop = random.nextInt(0, numberOfRows)
+        val yStop = random.nextInt(0, numberOfColumns)
+//
+//        stop_x = random.sample(range(0, number_of_rows), 1)[0]
+//        stop_y = random.sample(range(0, number_of_columns), 1)[0]
+//
+//        x_delta = stop_x - start_x
+//        y_delta = stop_y - start_y
+
+        val xDelta = xStop - xStart
+        val yDelta = yStop - yStart
+
+//        # TODO Check that this is the correct function to use
+//        angle = math.atan2(y_delta, x_delta)
+//        line_length = math.sqrt(x_delta * x_delta + y_delta * y_delta)
+        val angle = atan2(yDelta.toDouble(), xDelta.toDouble())
+        val length = sqrt(xDelta.toDouble() * xDelta + yDelta * yDelta)
+
+        sample.add(LinePrototypeWithAngle(com.kjipo.raster.segment.Pair(xStart, yStart), com.kjipo.raster.segment.Pair(xStop, yStop), angle, length))
+
+//
+//        training_samples[i, 0] = angle
+//        training_samples[i, 1] = line_length
+//        training_samples[i, 2] = start_x
+//        training_samples[i, 3] = start_y
+//        training_samples[i, 4] = stop_x
+//        training_samples[i, 5] = stop_y
+//
+//        if include_rectangle:
+//        training_samples[number_of_random_lines:(number_of_random_lines + 4), :] = add_rectangle(number_of_rows,
+//        number_of_columns)
+
+        if(includeRectangle) {
+            sample.addAll(addRectangle(numberOfRows, numberOfColumns))
+        }
+
+        return sample
+    }
+
+
+    fun addRectangle(numberOfRows: Int, numberOfColumns: Int): List<LinePrototypeWithAngle> {
+
+//    def add_rectangle(number_of_rows: int = 64, number_of_columns: int = 64) -> npt.ArrayLike:
+//    radius = math.floor(random.sample(range(1, min([number_of_rows, number_of_columns])), 1)[0] / 2)
+
+        val radius = floor(random.nextInt(1, min(numberOfRows, numberOfColumns))/2.0).toInt()
+//
+//    x_centre = random.sample(range(radius, number_of_rows - radius), 1)[0]
+//    y_centre = random.sample(range(radius, number_of_columns - radius), 1)[0]
+
+        val xCenter = random.nextInt(radius, numberOfRows - radius)
+        val yCenter = random.nextInt(radius, numberOfColumns - radius)
+
+//
+//    rotation = random.random() * math.pi / 2
+//    point_rotation = (math.pi / 2 - rotation) * random.random()
+
+        val rotation = random.nextDouble() * PI / 2.0
+        val pointRotation = (PI / 2.0 - rotation) * random.nextDouble()
+
+//
+//    first_point_rotation = rotation + point_rotation
+//    second_point_rotation = math.pi - point_rotation + rotation
+
+        val firstPointRotation = rotation + pointRotation
+        val secondPointRotation = PI - pointRotation + rotation
+
+//
+//    third_point_rotation = rotation - point_rotation
+//    fourth_point_rotation = rotation + math.pi + (math.pi - second_point_rotation + rotation)
+
+        val thirdPointRotation = rotation - pointRotation
+        val fourthPointRotation = rotation + PI + (PI - secondPointRotation + rotation)
+//
+//    x1 = math.cos(first_point_rotation) * radius + x_centre
+//    y1 = math.sin(first_point_rotation) * radius + y_centre
+
+        val x1 = (cos(firstPointRotation) * radius + xCenter).toInt()
+        val y1 = (sin(firstPointRotation) * radius + yCenter).toInt()
+
+//
+//    x2 = math.cos(second_point_rotation) * radius + x_centre
+//    y2 = math.sin(second_point_rotation) * radius + y_centre
+
+        val x2 = (cos(secondPointRotation) * radius + xCenter).toInt()
+        val y2 = (sin(secondPointRotation) * radius + yCenter).toInt()
+
+//
+//    x3 = math.cos(third_point_rotation) * radius + x_centre
+//    y3 = math.sin(third_point_rotation) * radius + y_centre
+
+        val x3 = (cos(thirdPointRotation) * radius + xCenter).toInt()
+        val y3 = (sin(thirdPointRotation) * radius + yCenter).toInt()
+//
+//    x4 = math.cos(fourth_point_rotation) * radius + x_centre
+//    y4 = math.sin(fourth_point_rotation) * radius + y_centre
+
+        val x4 = (cos(fourthPointRotation) * radius + xCenter).toInt()
+        val y4 = (sin(fourthPointRotation) * radius + yCenter).toInt()
+
+//
+//    length_12 = math.sqrt(math.pow(abs(x2 - x1), 2) + math.pow(abs(y2 - y1), 2))
+//    angle_12 = rotation + math.pi
+
+        val length12 = sqrt(abs(x2 - x1).toDouble().pow(2.0) + abs(y2 - y1).toDouble().pow(2.0))
+        val angle12 = rotation + PI
+
+//
+//    length_24 = math.sqrt(math.pow(abs(x2 - x4), 2) + math.pow(abs(y2 - y4), 2))
+//    angle_24 = rotation - math.pi / 2
+
+        val length24 = sqrt(abs(x2 - x4).toDouble().pow(2.0) + abs(y2 - y4).toDouble().pow(2.0))
+        val angle24 = rotation - PI / 2.0
+
+//
+//    length_13 = math.sqrt(math.pow(abs(x3 - x1), 2) + math.pow(abs(y3 - y1), 2))
+//    angle_13 = rotation - math.pi / 2
+
+        val length13 = sqrt(abs(x3 - x1).toDouble().pow(2.0) + abs(y3 - y1).toDouble().pow(2.0))
+        val angle13 = rotation - PI / 2.0
+
+//
+//    length_34 = math.sqrt(math.pow(abs(x3 - x4), 2) + math.pow(abs(y3 - y4), 2))
+//    angle_34 = math.pi + rotation
+
+        val length34 = sqrt(abs(x3 - x4).toDouble().pow(2.0) + abs(y3 - y4).toDouble().pow(2.0))
+        val angle34 = PI + rotation
+
+        return listOf(
+            LinePrototypeWithAngle(com.kjipo.raster.segment.Pair(x1, y1), com.kjipo.raster.segment.Pair(x2, y2), angle12, length12),
+            LinePrototypeWithAngle(com.kjipo.raster.segment.Pair(x2, y2), com.kjipo.raster.segment.Pair(x4, y4), angle24, length24),
+            LinePrototypeWithAngle(com.kjipo.raster.segment.Pair(x1, y1), com.kjipo.raster.segment.Pair(x3, y3), angle13, length13),
+            LinePrototypeWithAngle(com.kjipo.raster.segment.Pair(x3, y3), com.kjipo.raster.segment.Pair(x4, y4), angle34, length34))
+
+//
+//    return np.array([[angle_12, length_12, x1, y1, x2, y2],
+//    [angle_24, length_24, x2, y2, x4, y4],
+//    [angle_13, length_13, x1, y1, x3, y3],
+//    [angle_34, length_34, x3, y3, x4, y4]])
+
+
+    }
+
+
+}
---
+++ experiments/src/main/kotlin/com/kjipo/experiments/FindSimilarLines.kt
@@ -0,0 +1,191 @@
+package com.kjipo.experiments
+
+import com.kjipo.datageneration.CreateSamples
+import com.kjipo.datageneration.LinePrototypeWithAngle
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.min
+
+class SimilarSamples() {
+
+
+}
+
+
+class SearchDescription {
+
+
+}
+
+data class LinePair(val sampleId: Int, val line1Index: Int, val line2Index: Int, val linePairDescription: LinePairDescription)
+
+data class LinePairDescription(
+    val angleDiff: Double, val midPointRowLine1: Double, val midPointColumnLine1: Double,
+    val midPointRowLine2: Double, val midPointColumnLine2: Double
+)
+
+
+object FindSimilarLines {
+
+
+    private fun describeLinePair(line1: LinePrototypeWithAngle, line2: LinePrototypeWithAngle): LinePairDescription {
+//        angle_diff = abs(line1[0] - line2[0])
+
+        val angleDiff = (line1.angle - line2.angle).let { diff ->
+            if (diff > 2 * PI) {
+                diff - 2 * PI
+            } else if (diff < 0) {
+                diff + 2 * PI
+            } else {
+                diff
+            }
+        }
+
+//        if angle_diff > 2 * math.pi:
+//        angle_diff -= 2 * math.pi
+//        elif angle_diff < 0:
+//        angle_diff += 2 * math.pi
+//
+//        midpoint_x_line1 = line1[1] if line1[1] < line1[3] else line1[3] + abs(line1[1] - line1[3]) / 2
+//        midpoint_x_line2 = line2[1] if line2[1] < line2[3] else line2[3] + abs(line2[1] - line2[3]) / 2
+
+        val midPointColumnLine1 =
+            min(line1.startPair.column, line1.endPair.column) + abs(line1.startPair.column - line1.endPair.column) / 2.0
+        val midPointColumnLine2 =
+            min(line2.startPair.column, line2.endPair.column) + abs(line2.startPair.column - line2.endPair.column) / 2.0
+
+        val midPointRowLine1 =
+            min(line1.startPair.row, line1.endPair.row) + abs(line1.startPair.row - line1.endPair.row) / 2.0
+        val midPointRowLine2 =
+            min(line2.startPair.row, line2.endPair.row) + abs(line2.startPair.row - line2.endPair.row) / 2.0
+
+//        midpoint_y_line1 = line1[2] if line1[2] < line1[4] else line1[4] + abs(line1[2] - line1[4]) / 2
+//        midpoint_y_line2 = line2[3] if line2[2] < line2[4] else line2[4] + abs(line2[2] - line2[4]) / 2
+
+        return LinePairDescription(
+            angleDiff, midPointRowLine1, midPointColumnLine1,
+            midPointRowLine2, midPointColumnLine2
+        )
+
+    }
+
+
+    private fun findSimilarPaths(prototypesWithAngles: List<LinePrototypeWithAngle>) {
+//        '''
+//
+//        :param test_sample:
+//        :param indices_of_lines_to_use
+//        :param data: An array where the columns are angle difference, midpoint x difference, midpoint y difference,
+//        sample index, line 1 index within sample and line 2 index within sample
+//        :return:
+//        '''
+//        current_index = indices_of_lines_to_use[0]
+//        similar_samples: SimilarSamples = SimilarSamples(input_sample_id, test_sample, indices_of_lines_to_use)
+//
+//        similar_samples.input_sample_id = input_sample_id
+//
+//        search_description = SearchDescription()
+//        search_description.test_sample = test_sample
+//        search_description.similar_samples = similar_samples
+//
+//        for line_index in indices_of_lines_to_use[1:]:
+//        search_step = SearchStep()
+//        search_description.search_steps.append(search_step)
+//
+//        search_step.index_of_first_line = current_index
+//        search_step.index_of_second_line = line_index
+//
+//        # Describe the relation between two lines
+//                first_line_in_relation = test_sample[current_index]
+//        second_line_in_relation = test_sample[line_index]
+//
+//        search_step.first_line_in_relation = first_line_in_relation
+//        search_step.second_line_in_relation = second_line_in_relation
+//
+//        (angle_diff, midpoint_x_diff, midpoint_y_diff) = describe_two_lines(first_line_in_relation,
+//        second_line_in_relation)
+//        # Look for similar relations between lines in the data set
+//                (row_indices_of_closest_lines_across_lookup_examples, first_distances) = find_closest_lines_in_data(angle_diff,
+//        midpoint_x_diff,
+//        midpoint_y_diff,
+//        data,
+//        number_of_closest_lines_to_return=10)
+//        search_step.row_indices_of_closest_lines_across_lookup_examples = row_indices_of_closest_lines_across_lookup_examples
+//        search_step.first_distances = first_distances
+//
+//        counter = 0
+//        for index in row_indices_of_closest_lines_across_lookup_examples:
+//        row = data[index]
+//        # row[3] is the sample ID
+//        # row[4] is the ID of the last path element
+//        # row[5] is the ID of the new to-element
+//        paths_to_extend = similar_samples.find_paths_where_last_step_is_matching(int(row[3]), int(row[4]),
+//            int(row[5]))
+//        search_step.paths_to_extend = paths_to_extend
+//
+//        if len(paths_to_extend) == 0:
+//        if not similar_samples.exists_path_starting_with_id(int(row[3])):
+//        similar_samples.start_new_path(int(row[3]), int(row[4]), int(row[5]), first_distances[counter])
+//        else:
+//        for path in paths_to_extend:
+//        similar_samples.add_path_with_one_more_element(path, int(row[5]),
+//            first_distances[counter])
+//
+//        counter += 1
+//
+//        current_index = line_index
+
+
+    }
+
+
+    private fun describeLinePairs(sampleId: Int, lineSample: List<LinePrototypeWithAngle>): MutableList<LinePair> {
+        val linePairs = mutableListOf<LinePair>()
+        var index = 0
+        for(line in lineSample) {
+            var index2 = 0
+            for(line2 in lineSample) {
+                linePairs.add(LinePair(sampleId, index, index2, describeLinePair(line, line2)))
+            }
+        }
+        return linePairs
+    }
+
+
+    private fun findClosestLinesInData(linePairDescription: LinePairDescription, linePairDescriptionsInOtherSamples: List<LinePairDescription>) {
+//        '''
+//        Returns a tuple where the first array contains the indices of the closest lines, and the
+//        second array contains the distances.
+//        '''
+//        # TODO Using midpoint diff will not work for finding rectangles
+//                angle_diffs = abs(angle_diff - _data[:, 0])
+//        sorted_angle_diffs_indices = angle_diffs.argsort()
+//
+//        # Return the indices of the smallest angle differences
+//                return (sorted_angle_diffs_indices[0:number_of_closest_lines_to_return],
+//        angle_diffs[sorted_angle_diffs_indices[0:number_of_closest_lines_to_return]])
+
+        linePairDescriptionsInOtherSamples.map {linePair ->
+            val angleDiff = abs(linePairDescription.angleDiff - linePair.angleDiff)
+
+            // TODO
+
+
+        }
+
+
+    }
+
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sample = CreateSamples.generateSample(10, true, 64, 64, 10)
+
+        val description = describeLinePairs(1, sample)
+
+        println("Description: $description")
+
+    }
+
+
+}
